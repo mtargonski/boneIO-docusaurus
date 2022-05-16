7f68c8fd5f89980aaf0df78b20bbb41b2ed9393c
Paweł Szafer <pszafer@gmail.com>
2022-05-16 21:25:51 +0200

    remove .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/poznan.tbhydro.net/admin/ProjektyPrywatne/bone/docasaurus/node_modules/infima/dist/css/default/default.css'),
-  require('/home/poznan.tbhydro.net/admin/ProjektyPrywatne/bone/docasaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/poznan.tbhydro.net/admin/ProjektyPrywatne/bone/docasaurus/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/home/poznan.tbhydro.net/admin/ProjektyPrywatne/bone/docasaurus/src/css/custom.css'),
-];
